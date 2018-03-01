283837f6ba6dfe36d4f777efafb03a5b468ca81f
bangnx1 <bangnx1@fsoft.com.vn>
2018-03-01 22:27:01 +0700

    update code

--- vote_project/app.js
+++ vote_project/app.js
@@ -21,7 +21,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(session({
   secret : "secret",
   saveUninitialized: true,
--- vote_project/routes/index.js
+++ vote_project/routes/index.js
@@ -50,4 +50,31 @@ router.get('/log_out' , function(req, res, next) {
   res.redirect('/');
 });
 
+router.get('/details/:id', function(req, res, next) {
+  /** 
+   * Check when user login to fb from session
+  */
+ let user_id = req.session.user_id;
+ //---------------------------------------------
+ if(user_id) {
+  db_account.findOne({
+    where : {
+      id_user: user_id
+    }
+    }).then(account => {
+        res.render('details' , {
+          title : 'Mic On',
+          user_name : account.fullName,
+          link_login_or_logout : '/log_out',
+          data_video_array : video_array
+        });
+    }).catch(function (err) {
+      console.log(err);
+    });
+ //---------------------------------------------
+  } else {
+    res.render('details', { title: 'Mic On' , user_name : 'Login' , link_login_or_logout : '/authen/fb' });
+  }
+});
+
 module.exports = router;
