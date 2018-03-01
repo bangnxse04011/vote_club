982a056a395bc248ee7511221140fc6462f2cbcd
bangnx1 <bangnx1@fsoft.com.vn>
2018-03-01 21:21:04 +0700

    update code

--- vote_project/routes/index.js
+++ vote_project/routes/index.js
@@ -7,33 +7,41 @@ router.get('/', function(req, res, next) {
   /** 
    * Check when user login to fb from session
   */
- var video_array;
- db_manager_video.findAll({
-    plain: false
-  }).then(video => {
-    video_array = video.map((r) => (r.toJSON()));
-    console.log(video_array);
-  }).catch(function (err) {
-    console.log(err);
-  });
   let user_id = req.session.user_id;
+  //---------------------------------------------
   if(user_id) {
     db_account.findOne({
       where : {
         id_user: user_id
       }
     }).then(account => {
-      res.render('index' , {
-        title : 'Mic On - Medoly Club',
-        user_name : account.fullName,
-        link_login_or_logout : '/log_out',
-        data_video_array : video_array
+      db_manager_video.findAll({
+        plain: false
+      }).then(video => {
+        let video_array = video.map((r) => (r.toJSON()));
+        console.log(video_array);
+        res.render('index' , {
+          title : 'Mic On - Medoly Club',
+          user_name : account.fullName,
+          link_login_or_logout : '/log_out',
+          data_video_array : video_array
+        });
+      }).catch(function (err) {
+        console.log(err);
       });
     }).catch(function (err) {
       console.log(err);
     });
+  //---------------------------------------------
   } else {
-    res.render('index', { title: 'Mic On' , user_name : 'Login' , link_login_or_logout : '/authen/fb' , data_video_array : video_array });
+    db_manager_video.findAll({
+      plain: false
+    }).then(video => {
+      let video_array = video.map((r) => (r.toJSON()));
+      res.render('index', { title: 'Mic On' , user_name : 'Login' , link_login_or_logout : '/authen/fb' , data_video_array : video_array });
+    }).catch(function (err) {
+      console.log(err);
+    });
   }
 });
 
