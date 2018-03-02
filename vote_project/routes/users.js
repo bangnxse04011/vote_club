9cc20a4bce1e90eab3d9c90ee1496b77903fb04c
bangnx1 <bangnx1@fsoft.com.vn>
2018-03-02 22:27:29 +0700

    update code

--- vote_project/routes/users.js
+++ vote_project/routes/users.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var db_account = require('../public/js/db_table_account');
+var db_manager_video = require('../public/js/db_table_manager_video');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.end("okie" + req.session.user_id);
+});
+
+router.get('/find_by_name', function(req, res, next) {
+  let user_id = req.session.user_id;
+  let name_search = req.query.name_search;
+  db_manager_video.findAll({
+    where: {
+      full_name: {
+        ilike: '%' + name_search
+      }
+    },
+    plain: false
+  }).then(video => {
+    let video_array = video.map((r) => (r.toJSON()));
+    // res.render('details' , {
+    //   title : 'Mic On',
+    //   user_name : account.fullName,
+    //   link_login_or_logout : '/log_out',
+    //   data_video_array : video_array,
+    //   user_profile_video_view : user_profile_video
+    // });
+    console.log(video_array);
+  }).catch(function (err) {
+    console.log(err);
+  });
 });
 
 module.exports = router;
