037a410e1d6254ba25ea881150ef15539781d866
bangnx1 <bangnx1@fsoft.com.vn>
2018-03-05 22:44:04 +0700

    update code

--- vote_project/routes/admin.js
+++ vote_project/routes/admin.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var db_manager_video = require('../public/js/db_table_manager_video');
 var db_account_admin = require('../public/js/db_table_account_admin');
 var util = require('../public/js/unit');
 
@@ -29,23 +30,11 @@ router.post('/authen', function(req, res, next) {
     }).then(account => {
       let account_details = account.map((r) => (r.toJSON()));
       req.session.uname = uname;
-      // if(account == null || account == '' || account.length == 0) {
-      // db_account_admin.create({
-      //   fullName : "Nguyen Van Admin",
-      //   username: uname,
-      //   password : passwd,
-      //   status : 0
-      // });
       if(account.length > 0) {
         res.redirect("/admin/cp");
       } else {
         res.redirect("/admin/");
-      }
-      // } else {
-        // let active = account_details['status'];
-        // res.redirect("/cp");
-      // }
-      
+      }      
     }).catch(function (err) {
       console.log(err);
     });
@@ -56,7 +45,60 @@ router.get('/cp', function(req, res, next) {
   if(uname_session == null || uname_session == '' || uname_session == "") {
     res.redirect('/admin/');
   }
-  res.render('admin');
+  res.render("admin" , { message : "" });
 });
 
+/** 
+ * Method add account admin.
+*/
+router.post('/add_account' , function(req, res, next) {
+  let uname_session = req.session.uname;
+  let uname = req.body.uname;
+  let passwd = req.body.passwd;
+  if(uname_session == null || uname_session == '' || uname_session == "") {
+    res.redirect('/admin/');
+  }
+  db_account_admin.findAll({
+    where : {
+      username: uname,
+      password: passwd
+    }
+  }).then(account => {
+    let account_details = account.map((r) => (r.toJSON()));
+    req.session.uname = uname;
+    if(account_details.length <= 0 || account_details == '') {
+      db_account_admin.create({
+        username: uname,
+        password : passwd,
+        status : 0
+      });
+      res.redirect("/admin/cp");
+    } 
+    if(account_details.length > 0) {
+      res.redirect("/admin/");
+    }   
+  }).catch(function (err) {
+    console.log(err);
+  });
+});
+
+/** 
+ * Method add video
+*/
+router.get('/add_view' , function(req, res, next) {
+  let title_video = req.params['title_video'];
+  let id_video = req.params['id_video'];
+  let description = req.params['description'];
+  let uname_session = req.session.uname;
+  if(uname_session == null || uname_session == '' || uname_session == "") {
+    res.redirect('/admin/');
+  }
+  db_manager_video.create({
+    id_user: '1',
+    link_video : id_video,
+    description : description,
+    full_name : title_video
+  });
+  res.redirect("/admin/cp");
+});
 module.exports = router;
