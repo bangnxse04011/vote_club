b66539e780d3ce742005eee4d129b34c15dd3b77
bangnx1 <bangnx1@fsoft.com.vn>
2018-03-06 12:24:01 +0700

    update code

---
+++ vote_project/public/js/admin_manager.js
@@ -0,0 +1,24 @@
+/**
+ * Method invoke when page loaded
+ */
+$(document).ready(function(){
+    var list_data_video_manager;
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange=function(){
+        if (xhttp.readyState == 4 && xhttp.status == 200){
+          list_data_video_manager = xhttp.responseText;
+          var data_video = JSON.parse(list_data_video_manager);
+          let str = "";
+          for(var i = 0 ; i < data_video.length ; i++) {
+            str += "<option value='" + data_video[i]['link_video'] + "'/" + data_video[i]['id'] + ">" + data_video[i]['full_name'] + "</option>";
+          }
+          $('#link_video').html(str);
+        }
+    }
+    xhttp.open("GET","http://10.22.160.176:3000/admin/managers/find_all");
+    xhttp.send();
+
+    $("#link_video".click(function () {
+
+    }))
+  });
---
+++ vote_project/routes/admin_managers.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var db_manager_video = require('../public/js/db_table_manager_video');
+var util = require('../public/js/unit');
+
+/**
+ * Method find all DB
+ */
+router.get('/find_all', function(req, res, next) {
+    db_manager_video.findAll({
+        plain: false
+    }).then(video => {
+        let video_array = video.map((r) => (r.toJSON()));
+        res.end(JSON.stringify(video_array));
+    }).catch(function (err) {
+        console.log(err);
+    });
+});
+
+
+module.exports = router;
