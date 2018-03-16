b901c55c936724d3d6a5fe82cf5dfd9e0c2fd970
bangnx1 <bangnx1@fsoft.com.vn>
2018-03-16 17:02:35 +0700

    update code

--- vote_project/routes/admin_managers.js
+++ vote_project/routes/admin_managers.js
@@ -1,7 +1,32 @@
 var express = require('express');
 var router = express.Router();
 var db_manager_video = require('../public/js/db_table_manager_video');
+var db_account = require('../public/js/db_table_account');
 var util = require('../public/js/unit');
+var fs = require('fs');
+/**
+ * Create file users.txt
+ */
+router.get('/download_file', function(req, res, next) {
+    let uname_session = req.session.uname;
+    if(uname_session == null || uname_session == '' || uname_session == "") {
+        res.redirect('/admin/');
+    }
+    db_account.findAll({
+        plain: false
+    }).then(account => {
+        var file = fs.createWriteStream('data_users.csv');
+        file.on('error', function(err) {});
+        let account_details = account.map((r) => (r.toJSON()));
+        for (let index = 0; index < account_details.length; index++) {
+            file.write(account_details[index]['fullName'] + "," +  account_details[index]['email'] + ", https://www.facebook.com/" +  account_details[index]['id_user'] + '\r\n');
+        }
+        file.end();
+        res.end("Create file successfully.")
+    }).catch(function (err) {
+        console.log("error" + err);
+    });
+});
 
 /**
  * Method find all DB
